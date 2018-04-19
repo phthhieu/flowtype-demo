aba832f626787fa3df19c5e85e1936b14794bbcc
Hieu Pham <phthhieu@gmail.com>
2018-04-19 16:51:11 +0700

    Flowtype for enhancement

--- src/FlowDemo.js
+++ src/FlowDemo.js
@@ -1,13 +1,26 @@
 // @flow
 import React from 'react';
 
-const STEPS = {
-  step1: 'Add flow-bin',
-  step2: 'Run yarn flow init',
-  step3: 'Run yarn flow',
-};
-
 const FlowDemo = ({
+  step1,
+  step2,
+  step3,
+  summary,
+}: {
+  step1: string,
+  step2: string,
+  step3: string,
+  summary: string,
+}) => (
+  <ul>
+    <li>{step1}</li>
+    <li>{step2}</li>
+    <li>{step3}</li>
+    <li>{summary}</li>
+  </ul>
+);
+
+const enhance = Component => ({
   step1,
   step2,
   step3,
@@ -15,12 +28,14 @@ const FlowDemo = ({
   step1: string,
   step2: string,
   step3: string,
-}) => (
-  <ul>
-    <li>{step1}</li>
-    <li>{step2}</li>
-    <li>{step3}</li>
-  </ul>
-);
+}) => {
+  const newProps = {
+    summary: step1 + step2 + step3,
+    step1,
+    step2,
+    step3,
+  };
+  return <Component {...newProps} />;
+};
 
-export default FlowDemo;
+export default enhance(FlowDemo);
